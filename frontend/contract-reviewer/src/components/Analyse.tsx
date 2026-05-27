880a8351027eaa66fedde11eaece8ea0badc7182
raf945 <rafael@prchristensen.com>
2026-05-28 00:14:25 +0100

    updated api

--- frontend/contract-reviewer/src/components/Analyse.tsx
+++ frontend/contract-reviewer/src/components/Analyse.tsx
@@ -61,7 +61,7 @@ const handleClick = async (
   setIsAnalysing(true)
 
   try {
-    const response = await fetch(`/api/files/upload_pdf`, {
+    const response = await fetch(`/api/analyse/azure`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json;charset=UTF-8',
